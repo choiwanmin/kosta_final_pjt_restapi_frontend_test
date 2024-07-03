40d9edabb888f4c1aa04a201b2dae79616415c53
JinHaGyung <wlsgkrud96@gmail.com>
2024-07-03 17:25:25 +0900

    --test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/common/Header';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
