11e8106752a6162ca0ee42a4efaa67cf851d67dd
JinHaGyung <wlsgkrud96@gmail.com>
2024-07-03 16:53:38 +0900

    --main setting

---
+++ src/Router.js
@@ -0,0 +1,8 @@
+import { Routes,Route } from "react-router-dom";
+export default function Router(){
+    return(
+        <Routes>
+            {/* <Route path="/" element={}></Route> */}
+        </Routes>
+    )
+}
---
+++ src/components/common/Header.js
@@ -0,0 +1,24 @@
+import './header.css';
+
+function Header() {
+  return (
+      <nav className="w_bg nav_top fix_top">
+        <div className="header_wrapper">
+            <div className="flex_center header_logo">
+                <a className="logo_right" href="/">
+                    <div className="loginBoxTitle header_text">HRD SYSTEM</div>
+                </a>
+            </div>
+            <ul className="flex_center nav_right">
+                <li>유저이름</li>
+                <li>잡레벨</li>
+                <li >
+                    <img style={{borderRadius: "20px", width:"40px", height:"40px"}} />
+                </li>
+            </ul>
+        </div>
+     </nav>
+  );
+}
+
+export default Header;
