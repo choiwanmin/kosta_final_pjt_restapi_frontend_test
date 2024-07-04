a6b31933ef784dae2b2635c026a784229bd9a70c
rbgud4226 <hkh202857@gmail.com>
2024-07-04 11:59:15 +0900

    refactor:mainchat 수정

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import { Routes,Route } from "react-router-dom";
 import Login from "./components/user/Login";
 import Join from "./components/user/Join";
+import MainChat from "./components/chat/MainChat";
 
 export default function Router(){
     return(
         <Routes>
             <Route path="/login" element={<Login/>}></Route>
             <Route path="/join" element={<Join/>}></Route>
+            <Route path="/mainchat" element={<MainChat/>}/>
         </Routes>
     )
 }
