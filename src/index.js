1d9bef4a3725bdd32987be35daa8c40a35b2bbf1
JinHaGyung <wlsgkrud96@gmail.com>
2024-07-08 15:24:13 +0900

    ref:myrecord 페이지 완료

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import Header from './components/common/Header';
 import Leftnav from './components/common/Leftnav';
 import Router from './Router';
 import { BrowserRouter, Link } from "react-router-dom";
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Header/>
-    <Leftnav/>
-    <Router/>
- </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+        <Header/>
+        <Leftnav/>
+        <Router/>
+    </BrowserRouter>
+ </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
